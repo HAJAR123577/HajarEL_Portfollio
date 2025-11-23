6012be1edfb4a1ed051beb18f965d747b5b6a250
hajar <hajar.elmhedden2133@gmail.com>
2025-11-23 10:35:05 +0100

    ajout de la legende de l image world book

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -266,7 +266,7 @@ const Projects = () => {
             <div className="relative h-48 overflow-hidden">
               <img
                 src="/Photos/WorldBook.png"
-                alt="Portfolio personnel"
+                alt="World Book"
                 className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-slate-900/80 via-slate-900/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
